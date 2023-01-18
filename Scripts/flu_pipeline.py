709d980a26ad8e384d0fc7e2c20315f0a04cb290
Adrian Lima <adrianlimagaray@hotmail.com>
2023-01-18 12:07:26 -0500

    cleaning up code, adding comments

--- Scripts/flu_pipeline.py
+++ Scripts/flu_pipeline.py
@@ -49,17 +49,20 @@ class flu_pipeline() :
         #That needs to be moved back to the main network from Analysis PC
         #self.final_results_dir This is were i will dump the value of results into a file and the aligned sequences
     
+        #create Final Report for Epis
         create_final_report(run_date,self.nextclade_output,results,self.final_results_dir)
 
+        #Move fasta files into result dir
         move_fasta_files([*fastq_paths_dic],self.res_dir,self.final_results_dir, run_date )
 
+        #create alignment files per flu type tested
         create_alignment_file(self.dir_path,self.res_dir,results,self.final_results_dir,run_date)
         print("Alignment Files Created")
+        #create tree based on alignment files
         create_phylogentic_tree(self.final_results_dir,run_date)
         print("Phlyogenetic Tree Built")
 
-        #delete barcodes + IRMA data
-        
+        #delete barcodes + IRMA data + Nextclade
         clean_run_files(minion_path,self.res_dir,run_date,self.nextclade_output)
 
 
@@ -67,55 +70,11 @@ class flu_pipeline() :
 
 
 
-def pipeline(minion_path,sample_sheet_p): #variables, analysis_working_dir, final_out_dir, nextclade_output
-    
-    res_dir= "/home/ssh_user/FLU_WGS_Sequencing/IRMA/" #this will need a permant address
-
-    run_date = sample_sheet_p.split("/")[-1][:-4]
-
-    dir_path = "/".join(os.path.dirname(os.path.realpath(__file__)).split("/")[:-1]) #path minus scripts 
-    res_dir+=run_date
-    nextclade_output= "/home/ssh_user/FLU_WGS_Sequencing/Nextclade"
-    nextclade_output=nextclade_output+"/"+run_date
-
-    #Step 1 merge  fasta files
-    #fastq_paths_dic = merge_seq_fastq(minion_path,sample_sheet_p)
-
-    print("Merging Completing")
-
-    fastq_paths_dic={"2225102_060722_01" : "","2225196_060722_02" :""}
-
-    #Step 2 run irma for allignment
-    #irma_runner(fastq_paths_dic,dir_path,res_dir) 
-    #in the future function after IRMA will return String with where the files have been moved to
-    print("IRMA Completing")
-
-    #fastq_paths_dic={'2225196': '/home/ks_khel/Desktop/FLU_DATA/barcode02/2225196_combined.fastq.gz', '2231833': '/home/ks_khel/Desktop/FLU_DATA/barcode04/2231833_combined.fastq.gz', '2225102': '/home/ks_khel/Desktop/FLU_DATA/barcode01/2225102_combined.fastq.gz', '2229929': '/home/ks_khel/Desktop/FLU_DATA/barcode05/2229929_combined.fastq.gz', '2225184': '/home/ks_khel/Desktop/FLU_DATA/barcode03/2225184_combined.fastq.gz'}
-   # fastq_paths_dic={"2225102_060722_01" : "","2225196_060722_02" :"","2231833_060722_04" :" ","2225184_060722_03" :"",  "2229929_060722_05" :""}
-    
-    #Now will need to import demographics from horizon to our local db
-    #Step 3 Import Demographics
-    #has to be done at this step because this after HSN has been mapped
-    run_import_demo(dir_path,[*fastq_paths_dic])
-
-
-    #Step 4 run nextclade and return hits
-    #results=nextclade_runner([*fastq_paths_dic],res_dir,dir_path,nextclade_output)
-    
-    print("Nextclade Completed")
-
-    #next thing to do is to create GISAID Reports #epiFLU seems to be still down
-    #and variant reports
-    
-
-
-
 if __name__ == "__main__":
     
     dir_path = "/".join(os.path.dirname(os.path.realpath(__file__)).split("/")[:-1]) #path minus scripts 
-    sample_sheet_p ="/FLU_WGS_Sequencing/sample_sheet/060722.csv" #will need a way to pass this differently
- 
-    print(sys.argv)
+    
+    #print(sys.argv)
     input_path = sys.argv[1]
     sample_sheet_p = sys.argv[2]
     print(input_path)
@@ -126,15 +85,6 @@ if __name__ == "__main__":
     influenza_pipeline = flu_pipeline(dir_path)
        
     influenza_pipeline.run_flu_pipeline(input_path,sample_sheet_p)
-
-   # if sys.argv[0] == "" or sys.argv[0] == "/home/ssh_user/Documents/GitHub/Infulenza_Pipeline/Scripts/flu_pipeline.py":
-    #    input_path= input("Please enter the path to MinION data:     ")
-
-        #pipeline(input_path,sample_sheet_p)
-        
-     #   influenza_pipeline = flu_pipeline(dir_path)
-        
-      #  influenza_pipeline.run_flu_pipeline(input_path,sample_sheet_p)
 
 
 
